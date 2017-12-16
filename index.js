231e3e3543a12bcbc57e491f9a49d63560a84a6e
Kawin Glomjai <g.kawin@live.com>
2017-12-16 12:45:42 +0700

    wrapped by Native Promise
    init

---
+++ index.js
@@ -0,0 +1,38 @@
+var http = require('https')
+
+const p = new Promise((resolve, reject) => {
+  const request = http.request({
+    host: 'api-dev.axia.life',
+    path: '/api/chat/user/alluser',
+    port: 443,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, (res) => {
+    res.setEncoding('utf8')
+    res.setTimeout(10000)
+    if (res.statusCode === 404) { reject('server not found') }
+
+    var data = []
+    res.on('error', (e) => {
+      reject('problem with request: ' + e.message)
+    })
+    .on('data', (chunk) => {
+      data.push(chunk)
+    }).on('end', (e) => {
+      if (data.length > 1) {
+        const concatBuffData = Buffer.concat(data).toString()
+        resolve(concatBuffData)
+      } else {
+        resolve(JSON.parse(data))
+      }
+    })
+  })
+
+  request.on('error', function (e) {
+    reject('problem with request: ' + e.message)
+  })
+  request.write('data\n')
+  request.end()
+})
